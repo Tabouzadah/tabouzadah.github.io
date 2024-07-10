bfd827102ca5b36a26fcad484b7c0de64d94f626
Tabouzadah <seif.tabo@gmail.com>
2024-07-11 07:27:10 +0800

    include vercel speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,13 @@ import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Process from "@/components/Process";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
+        <SpeedInsights />
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
